4e02e8ab25c9b86fc641d37068b03ba5739f8a96
Mykatyk Xaadd <misha23089@outlook.com>
2022-06-14 22:09:59 +0300

    got rid of the artifact

--- 3d_graph/ini.cpp
+++ 3d_graph/ini.cpp
@@ -9,10 +9,10 @@ float camera_speed = 0.3;
 Vector3f light_dir = Graphic::Normalize(Vector3f(0.6, 0.75, -1.0));
 
 int max_reflect = 32;
-int samples = 1024;
+int samples = 256;
 int viewport_samples = 1;
 
-Vector3f camere_origin(10, -4, 5);
-Vector3f camere_rotation(0, 0,3.14);
+Vector3f camere_origin(0,0, 50);
+Vector3f camere_rotation(0, 3.14/2, 0);
 
 std::string render_path = "D:\\Ainstall\\render\\";
